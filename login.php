2532c5d5408dc451ca1871ba7989d5560b597383
Garrett <garrett@iron-point.com>
2014-06-24 20:24:27 -0600

    Created MSU Advizzze Login Page

---
+++ login.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<!--Author: Garrett, Jarrett, Reece
+    Date:   06/24/14
+    Purpose: To create an information system for students and advisors to interact and map a degree path.
+    -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+<title>MSU Student Advising Portal</title>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+
+
+
+
+</head>
+<body>
+    <h1>Login to MSU Advizzze</h1>
+    <form action="" method="post">
+        <input type="text" placeholder="User Name" value=""/>
+        <input type="text" placeholder="User Name" value=""/>
+    </form>
+</body>
+</html>
